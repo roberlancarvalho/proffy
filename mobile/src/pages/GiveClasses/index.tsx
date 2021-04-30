90ea0b6414d95c70ee6bb0ab76f66ee68e393551
roberlan <roberlan.carvalho@gmail.com>
2021-04-30 14:50:05 -0300

    Enviando projeto completo - primeiro commit

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './styles'
+
+function GiveClasses() {
+  return (
+
+    <View style={styles.container} >
+      <Text>Teste </Text>
+    </View>
+
+  )
+}
+ 
+export default GiveClasses
